93c37505ef61ea0a689748285ac732ec6d800d7e
mari-trancoso <mnegraotrancoso@gmail.com>
2022-09-08 13:43:08 -0300

    resolução

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,22 @@
-const num = //seu numero aqui
+const num = Number(prompt("Digite um número e descubra se ele é divisível por 2 ou por 3."))
 
+//1. ifs aninhados
+
+if (num % 2 === 0) {
+    console.log(`O número ${num} é divisível por 2.`)
+    if (num % 3 === 0) {
+        console.log(`O número ${num} é divisível por 3.`)
+    }
+} else if( num % 3 === 0){
+    console.log(`O número ${num} é divisível por 3.`)
+    //entra em múltiplos de 3 que não são pares
+} else {
+   console.log(`O número ${num} não é divisível de 2 ou de 3.`)
+    // entra em números que não são múltiplos de 2 ou 3
+}
+
+
+//2. operador lógico
+if ((num%2 === 0) || (num%3 === 0)){
+    console.log(`O número ${num} é divisível por 2 ou por 3.`)
+}
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,13 +1,20 @@
 //código a ser reescrito
 
-if(nome === "José"){
-	console.log("Oi, Zé!");
-} else {
-	console.log("Olá, "+nome);
-}
+let nome = prompt("Qual o seu nome?")
+let idade =prompt("Qual a sua idade?")
 
-if(idade >= 18){
-	console.log("pode tirar carteira de motorista!");
-} else {
-	console.log("Ainda não pode tirar carteira de motorista");
-}
+// if(nome === "José"){
+// 	console.log("Oi, Zé!");
+// } else {
+// 	console.log("Olá, "+nome);
+// }
+
+nome === "José" ? console.log("Oi, Zé!") : console.log("Olá, " + nome)
+
+// if(idade >= 18){
+// 	console.log("pode tirar carteira de motorista!");
+// } else {
+// 	console.log("Ainda não pode tirar carteira de motorista");
+// }
+
+idade >= 18 ? console.log("Pode tirar a carteira de motorista!") : console.log("Ainda não pode tirar a carteira de motorista.")
